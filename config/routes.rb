3747e7ba8eb3942f1b1cec7fc448e6a0510244dd
akino102819 <k.akino1998@icloud.com>
2020-03-24 13:59:24 +0900

    商品詳細ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     :sessions => 'users/sessions'
   }
   root "products#index"
-
+  resources :products, only: [:show]
   devise_scope :user do
     get "residences", :to => "users/registrations#new_residence"
     post "residences", :to => "users/registrations#create_residence"
