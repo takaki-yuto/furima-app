a416b21ffd2e6fcb923d74821de63693d7154ce3
takaki-yuuto <yutoction0206@gmail.com>
2020-03-30 14:52:32 +0900

    before_actionの定義

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,20 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show]
+
   def index
   end
 
   def show
-    @product = Product.find(params[:id])
     @images = @product.images
   end
 
   def new
   end
 
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+  
 end
