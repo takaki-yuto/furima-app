a068db172ed73a84edfac74c49da1e8023b1cc88
akino102819 <k.akino1998@icloud.com>
2020-04-07 18:52:42 +0900

    buyer修正

--- db/migrate/20200325052946_add_column_seller_fkey_to_products.rb
+++ db/migrate/20200325052946_add_column_seller_fkey_to_products.rb
@@ -1,6 +1,5 @@
 class AddColumnSellerFkeyToProducts < ActiveRecord::Migration[5.2]
   def change
     add_reference :products, :seller, foreign_key: { to_table: :users }
-    add_reference :products, :buyer, foreign_key: { to_table: :users }
   end
 end
